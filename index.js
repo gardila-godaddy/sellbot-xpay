be1b403d670eef61289309ed7b13fa62b1fc5e09
German Ardila <109693958+gardila-godaddy@users.noreply.github.com>
2024-07-08 20:59:48 -0400

    get port from env variable

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
+require('dotenv').config()
+
 app.set("view engine", "pug");
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -16,7 +19,7 @@ app.get("/", (_req, res) => {
 app.use("/link", require("./routes/link"));
 app.use("/collect", require("./routes/collect"));
 
-const port = 1347;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Sellbot listening at ${port}`);
 });
